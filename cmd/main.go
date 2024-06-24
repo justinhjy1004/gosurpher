697f8c35d5a5d66d22ecb65f2306bf4677222876
Justino <justinhjy1004@gmail.com>
2024-06-24 17:53:37 -0400

    initial changes

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"gosurpher/views"
+
+	"github.com/a-h/templ" 
+	"github.com/labstack/echo/v4"
+)
+
+func homeHandler(c echo.Context) error {
+	
+	return render(c, views.Home("Page Title"))
+	
+}
+
+func render(c echo.Context, cmp templ.Component) error {
+	// This is real
+	return cmp.Render(c.Request().Context(), c.Response())
+
+}
+
+func main() {
+
+	e := echo.New()
+	e.GET("/", homeHandler)
+	e.Logger.Fatal(e.Start(":42069"))
+
+}
