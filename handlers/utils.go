d72a4cbd1ec082411c105c926ba87a67225156bd
justinhjy1004 <justinhjy1004@gmail.com>
2024-06-27 15:32:56 -0400

    last changes

---
+++ handlers/utils.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
+
+func Render(c echo.Context, cmp templ.Component) error {
+
+	return cmp.Render(c.Request().Context(), c.Response())
+
+}
