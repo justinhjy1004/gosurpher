2ff400b0415a6e25c973f0d2e2fdaa7d44d3a81f
justinhjy1004 <justinhjy1004@gmail.com>
2024-06-30 07:29:23 -0400

    major changes

--- views/layout_templ.go
+++ views/layout_templ.go
@@ -57,7 +57,7 @@ func Layout(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</main><footer class=\"bg-white rounded-lg shadow m-4 dark:bg-gray-800\"><div class=\"w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between\"><span class=\"text-sm text-gray-500 sm:text-center dark:text-gray-400\">© 2024 GoSurpher. All Rights Reserved.</span><ul class=\"flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0\"></ul></div></footer></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</main><footer class=\"bg-white rounded-lg p-4 shadow m-4 dark:bg-gray-800\"><div class=\"w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between\"><span class=\"text-sm text-gray-500 sm:text-center dark:text-gray-400\">© 2024 GoSurpher. All Rights Reserved.</span></div></footer></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
