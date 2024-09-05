f8fc3d6fdaf77319996d9b4568737c9752d78b2c
justinhjy1004 <justinhjy1004@gmail.com>
2024-09-05 10:33:04 -0400

    changed path

--- db/database.go
+++ db/database.go
@@ -6,16 +6,15 @@ import (
 	"gosurpher/models"
 	"log"
 	"math/rand"
-	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 /* This handles the relevant function calls
 for the database for the application */
 
-var db_type string = "mysql"
-var db_path string = os.Getenv("MYSQL_DATABASE")
+var db_type string = "sqlite3"
+var db_path string = "./db/gosurpher.db"
 
 // Select all blogs and return all the content in them
 func Select_blogs() []models.Blog {
